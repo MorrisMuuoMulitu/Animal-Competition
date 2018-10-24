c921cc785e6419fa66aa2f2292879346935346e2
student <student@itlab.pmmk.pte.hu>
2018-10-24 08:12:37 +0200

    First commit

---
+++ Animal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCompetition
+{
+    class Animal
+    {
+        //fields
+        private String name;
+        private int birthYear;
+
+        private int beautyPoint;
+        private int behaviourPoint;
+        private int score;
+
+        private static int actualYear;
+        private static int ageLimit;
+
+        //constructor
+        public Animal(String name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+        //methods
+        public int Age()
+        {
+            return actualYear - birthYear;
+        }
+        public void Scoring(int beautyPoint,int behaviourPoint)
+        {
+            this.beautyPoint = beautyPoint;
+            this.behaviourPoint = behaviourPoint;
+            if (Age()<ageLimit)
+            {
+                score = this.behaviourPoint * Age() + this.beautyPoint * (ageLimit - Age());
+
+            }
+            else
+            {
+                score = 0;
+            }
+        }
+        public override string ToString()
+        {
+            return name+" score:"+score+"point (beauty: "+beautyPoint+",behaviour : "+behaviourPoint+")";
+        }
+        //properties
+        public string Name
+        {
+            get { return name; }
+        }
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+        public int BeautyPoint
+        {
+            get { return beautyPoint; }
+        }
+        public int BehaviourPoint
+        {
+            get { return behaviourPoint; }
+        }
+        public int Score
+        {
+            get { return score; }
+        }
+        //their value can be modified from outside
+        public static int ActualYear
+        {
+            get { return actualYear; }
+            set { actualYear = value; }
+        }
+        public static int AgeLimit
+        {
+            get { return ageLimit; }
+            set { ageLimit = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int actYear = 2018, ageLimit = 10;
+            Animal.ActualYear = actYear;
+            Animal.AgeLimit = ageLimit;
+
+            Competition();
+        }
+
+        private static void Competition()
+        {
+            Animal animal;
+            string name;
+            int birthYear;
+            int beauty,behaviour;
+            int randomScoreLimit = 10;
+
+            Random rand = new Random();
+
+            int competitorSum = 0;
+            int scoreSum = 0;
+            //int maxScore = 0;
+
+            Console.WriteLine("Competition begins \n");
+            char next = 'y';
+
+            while (next== 'y')
+            {
+                Console.Write("\n The name of the animal: ");
+                name = Console.ReadLine();
+                Console.Write("Its birthYear: ");
+                while (!int.TryParse(Console.ReadLine(),out birthYear) || birthYear<=0 || birthYear>= Animal.ActualYear )
+                {
+                    Console.Write("Incorrect data,try again");
+             
+                }
+                beauty = rand.Next(randomScoreLimit + 1);
+                behaviour = rand.Next(randomScoreLimit + 1);
+                animal = new Animal(name, birthYear);
+                animal.Scoring(beauty, behaviour);
+                Console.WriteLine(animal);
+                scoreSum += animal.Score;
+                competitorSum++;
+                Console.Write("Is there any more animal? y/n: ");
+                next = char.Parse(Console.ReadLine());
+
+                //Try find the maximum
+            }
+        }
+    }
+}
